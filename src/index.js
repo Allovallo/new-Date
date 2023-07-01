dbf3dd76fd71a92b5d5ddeab1f89a243551ca840
Allovallo <30566134+Allovallo@users.noreply.github.com>
2023-07-01 16:42:57 +0300

    Updated files
    Updated files

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// let user = {
+//   name: "John",
+//   age: 30,
+
+//   toString() {
+//     return `{name: "${this.name}", age: ${this.age}}`;
+//   }
+// };
+
+// console.log(user);
+
+
+// let student = {
+//   name: 'John',
+//   age: 30,
+//   isAdmin: false,
+//   courses: ['html', 'css', 'js'],
+//   wife: null
+// };
+
+// let json = JSON.stringify(student);
+// console.log(json);
+
+// let user = {
+//     sayHi() {
+//         console.log("Hello");
+//     },
+//     [Symbol("id")]: 123,
+//     something: undefined,
+// }
+
+// console.log(JSON.stringify(user));
+
